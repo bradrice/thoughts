a1022d0bed6d4ba71f649fb70ef72464960eb25f
Brad Rice <bradrice1@gmail.com>
2014-09-20 00:42:34 +0000

    added rewrite base to .hcaccess

--- publishconf.py
+++ publishconf.py
@@ -12,6 +12,7 @@ from pelicanconf import *
 
 SITEURL = 'http://bradrice.com/thoughts'
 RELATIVE_URLS = False
+FEED_DOMAIN = SITEURL
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
